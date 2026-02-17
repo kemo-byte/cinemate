f556d908c7f65438bcbd88f8fd265c5e1e944fdb
kamal kafi <kamalkafi12@gmail.com>
2026-02-17 06:44:40 +0200

    feat: add links.txt, Header, Footer, and update MovieList
    add links to external APIs, add Header and Footer components to App, and update MovieList component with styling.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { AllRoutes } from './routes/AllRoutes';
 import './App.css';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <AllRoutes />
+      <Header />
+        <AllRoutes />
+      <Footer />
     </div>
   );
 }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -2,7 +2,7 @@
 export const MovieList = () => {
   return (
     <main>
-      <div className="text-9xl">
+      <div className="text-9xl bg-emerald-200 p-10 border-8 border-blue-700 rounded-lg m-5">
         MovieList
       </div>
     </main>
