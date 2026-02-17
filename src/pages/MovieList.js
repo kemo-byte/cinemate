ca3629366ff3e9488e55bc4308ca36835c3f43a2
kamal kafi <kamalkafi12@gmail.com>
2026-02-17 13:58:07 +0200

    feat: implement dynamic Card data and movie list fetching
    implement dynamic Card data and movie list fetching from themoviedb api. add Backup image for movies without poster_path.

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,10 +1,26 @@
-import { Card } from '../components';
+import { useEffect } from "react";
+import { useState } from "react";
+import { Card } from "../components";
+
 export const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovies(){
+      const response = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=f19499b5c289c6e4e99d418d2a0e077e");
+      const data = await response.json();
+      setMovies(data.results);
+    }
+    fetchMovies();
+  }, [])
+
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
-        <div className="flex justify-start flex-wrap">
-          <Card />
+        <div className="flex justify-start flex-wrap">       
+          { movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          )) }          
         </div>
       </section>
     </main>
